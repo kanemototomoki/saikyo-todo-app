1a3582ddae01e7a854d25f12ae07d891d2be31d0
kanemototomoki <kanemototomoki@gmail.com>
2023-03-05 22:39:24 +0900

    update: functionsをbuildに含める

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
@@ -8,5 +10,16 @@ export default defineConfig({
     minify: true,
     outDir: './pages'
   },
-  plugins: [tsconfigPaths(), react()]
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: path.resolve(__dirname, './functions') + '/[!.]*',
+          dest: './functions/'
+        }
+      ]
+    })
+  ]
 })
